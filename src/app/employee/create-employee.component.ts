40db6c62c8a5cf5a3217b0465071e187a231ffd0
Saeed Murrad <smurad@pionlog.com>
2019-12-04 16:05:46 +0500

    FormGroup and FormControl in Reactive Forms end with validations.

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 // Import FormGroup and FormControl classes
-import { FormGroup, FormBuilder,Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { CustomValidators } from '../shared/custom.validators';
 @Component({
   selector: 'app-create-employee',
   templateUrl: './create-employee.component.html',
@@ -8,40 +9,143 @@ import { FormGroup, FormBuilder,Validators } from '@angular/forms';
 })
 export class CreateEmployeeComponent implements OnInit {
 
-   // This FormGroup contains fullName and Email form controls
-   employeeForm: FormGroup;
+ // Group properties on the formErrors object. The UI will bind to these properties
+// to display the respective validation messages
+formErrors = {
+  'fullName': '',
+  'email': '',
+  'confirmEmail': '',
+  'emailGroup': '',
+  'phone': '',
+  'skillName': '',
+  'experienceInYears': '',
+  'proficiency': ''
+};
 
-   constructor(private fb: FormBuilder) { }
- 
-   // Initialise the FormGroup with the 2 FormControls we need.
-   // ngOnInit ensures the FormGroup and it's form controls are
-   // created when the component is initialised
-   ngOnInit() {
+// This structure stoes all the validation messages for the form Include validation
+// messages for confirmEmail and emailGroup properties. Notice to store the
+// validation message for the emailGroup we are using emailGroup key. This is the
+// same key that the matchEmails() validation function below returns, if the email
+// and confirm email do not match.
+validationMessages = {
+  'fullName': {
+    'required': 'Full Name is required.',
+    'minlength': 'Full Name must be greater than 2 characters',
+    'maxlength': 'Full Name must be less than 10 characters.',
+  },
+  'email': {
+    'required': 'Email is required.',
+    'emailDomain': 'Email domian should be dell.com'
+  },
+  'confirmEmail': {
+    'required': 'Confirm Email is required.'
+  },
+  'emailGroup': {
+    'emailMismatch': 'Email and Confirm Email do not match.'
+  },
+  'phone': {
+    'required': 'Phone is required.'
+  },
+  'skillName': {
+    'required': 'Skill Name is required.',
+  },
+  'experienceInYears': {
+    'required': 'Experience is required.',
+  },
+  'proficiency': {
+    'required': 'Proficiency is required.',
+  },
+};
+
+  // This FormGroup contains fullName and Email form controls
+  employeeForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  // Initialise the FormGroup with the 2 FormControls we need.
+  // ngOnInit ensures the FormGroup and it's form controls are
+  // created when the component is initialised
+  ngOnInit() {
     this.employeeForm = this.fb.group({
-      fullName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(10)]] ,
-      email: [''],
+      fullName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
+      contactPreference: ['email'],
+      emailGroup: this.fb.group({
+        email: ['', [Validators.required, CustomValidators.emailDomain('dell.com')]],
+        confirmEmail: ['', [Validators.required]],
+      }, { validator: matchEmails }),
+      phone: [''],
       skills: this.fb.group({
-        skillName: [''],
-        experienceInYears: [''],
-        proficiency: ['beginner']
+        skillName: ['', Validators.required],
+        experienceInYears: ['', Validators.required],
+        proficiency: ['', Validators.required]
       }),
     });
-   }
-   onSubmit(): void {
+  
+    this.employeeForm.valueChanges.subscribe((data) => {
+      this.logValidationErrors(this.employeeForm);
+    });
+  
+    this.employeeForm.get('contactPreference').valueChanges.subscribe((data: string) => {
+      this.onContactPrefernceChange(data);
+    });
+  }
+  onSubmit(): void {
     console.log(this.employeeForm.value);
   }
-
-  onLoadDataClick(): void {
-    //.patchValue() can be used for updating partial controls
-    this.employeeForm.setValue({
-      fullName: 'Saeed Murrad',
-      email: 'saeedmurrad@gmail.com',
-      skills: {
-        skillName: 'Java',
-        experienceInYears: 3,
-        proficiency: 'intermediate'
+  // If the Selected Radio Button value is "phone", then add the
+  // required validator function otherwise remove it
+  onContactPrefernceChange(selectedValue: string) {
+    const phoneFormControl = this.employeeForm.get('phone');
+    if (selectedValue === 'phone') {
+      phoneFormControl.setValidators(Validators.required);
+    } else {
+      phoneFormControl.clearValidators();
+    }
+    phoneFormControl.updateValueAndValidity();
+  }
+  logValidationErrors(group: FormGroup = this.employeeForm): void {
+    Object.keys(group.controls).forEach((key: string) => {
+      const abstractControl = group.get(key);
+      this.formErrors[key] = '';
+      // Loop through nested form groups and form controls to check
+      // for validation errors. For the form groups and form controls
+      // that have failed validation, retrieve the corresponding
+      // validation message from validationMessages object and store
+      // it in the formErrors object. The UI binds to the formErrors
+      // object properties to display the validation errors.
+      if (abstractControl && !abstractControl.valid
+        && (abstractControl.touched || abstractControl.dirty)) {
+        const messages = this.validationMessages[key];
+        for (const errorKey in abstractControl.errors) {
+          if (errorKey) {
+            this.formErrors[key] += messages[errorKey] + ' ';
+          }
+        }
+      }
+  
+      if (abstractControl instanceof FormGroup) {
+        this.logValidationErrors(abstractControl);
       }
     });
   }
 
+  onLoadDataClick(): void {
+    console.log(this.formErrors);
+  }
+
+}
+// Nested form group (emailGroup) is passed as a parameter. Retrieve email and
+// confirmEmail form controls. If the values are equal return null to indicate
+// validation passed otherwise an object with emailMismatch key. Please note we
+// used this same key in the validationMessages object against emailGroup
+// property to store the corresponding validation error message
+function matchEmails(group: AbstractControl): { [key: string]: any } | null {
+  const emailControl = group.get('email');
+  const confirmEmailControl = group.get('confirmEmail');
+
+  if (emailControl.value === confirmEmailControl.value || confirmEmailControl.pristine) {
+    return null;
+  } else {
+    return { 'emailMismatch': true };
+  }
 }
