0c4b3b3d3075138f53bfd3a0063b84a397083080
Saeed Murrad <smurad@pionlog.com>
2019-12-05 17:58:30 +0500

    Created Employee Feature Module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,28 @@
+// app.module.ts
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CreateEmployeeComponent } from './employee/create-employee.component';
-import { ListEmployeesComponent } from './employee/list-employees.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { EmployeeModule } from './employee/employee.module';
+
 import { EmployeeService } from './employee/employee.service';
-import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
 import { HomeComponent } from './home.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreateEmployeeComponent,
-    ListEmployeesComponent,
     HomeComponent,
     PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    EmployeeModule
   ],
   providers: [EmployeeService],
   bootstrap: [AppComponent]
---
+++ src/app/employee/employee.module.ts
@@ -0,0 +1,35 @@
+// employee.module.ts
+import { NgModule } from '@angular/core';
+// Exports all the basic Angular directives and pipes
+// such as NgIf, NgFor, DecimalPipe etc.
+import { CommonModule } from '@angular/common';
+// CreateEmployeeComponent uses ReactiveFormsModule directives such as
+// formGroup so ReactiveFormsModule needs to be imported into this Module
+// An alternative approach would be to create a Shared module and export
+// the ReactiveFormsModule from it, so any other module that needs
+// ReactiveFormsModule can import it from the SharedModule.
+import { ReactiveFormsModule } from '@angular/forms';
+
+// Import and declare the components that belong to this Employee Module
+import { CreateEmployeeComponent } from './create-employee.component';
+import { ListEmployeesComponent } from './list-employees.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  declarations: [
+    CreateEmployeeComponent,
+    ListEmployeesComponent
+  ],
+  // If you want the components that belong to this module, available to
+  // other modules, that import this module, then include all those
+  // components in the exports array. Similarly you can also export the
+  // imported Angular Modules
+  // exports: [
+  //   CreateEmployeeComponent,
+  //   ReactiveFormsModule
+  // ]
+})
+export class EmployeeModule { }
