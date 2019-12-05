1dd196c42dd09326f3dcdacade0a5319368d10c7
Saeed Murrad <smurad@pionlog.com>
2019-12-05 18:41:26 +0500

    Implemented shared module concept

--- src/app/employee/employee.module.ts
+++ src/app/employee/employee.module.ts
@@ -1,26 +1,17 @@
 // employee.module.ts
 import { NgModule } from '@angular/core';
-// Exports all the basic Angular directives and pipes
-// such as NgIf, NgFor, DecimalPipe etc.
-import { CommonModule } from '@angular/common';
-// CreateEmployeeComponent uses ReactiveFormsModule directives such as
-// formGroup so ReactiveFormsModule needs to be imported into this Module
-// An alternative approach would be to create a Shared module and export
-// the ReactiveFormsModule from it, so any other module that needs
-// ReactiveFormsModule can import it from the SharedModule.
-import { ReactiveFormsModule } from '@angular/forms';
 // Import the EmployeeRoutingModule
 import { EmployeeRoutingModule } from './employee-routing.module';
 
 // Import and declare the components that belong to this Employee Module
 import { CreateEmployeeComponent } from './create-employee.component';
 import { ListEmployeesComponent } from './list-employees.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    EmployeeRoutingModule
+    EmployeeRoutingModule,
+    SharedModule
   ],
   declarations: [
     CreateEmployeeComponent,
